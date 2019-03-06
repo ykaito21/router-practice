08ca5ff2e39976161c71ed758fa8bee5f3036298
Yuki Kaito <ykaito21@gmail.com>
2019-03-06 21:19:14 +0900

    basic route and nav

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import Players from "./Players";
+import Teams from "./Teams";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>good</h1>
-      </div>
+      <Router>
+        <div>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/players" component={Players} />
+            <Route exact path="/teams" component={Teams} />
+            <Route
+              render={() => <h1 className="text-center">404 Page not found</h1>}
+            />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/Players.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Players extends Component {
+  render() {
+    return (
+      <div className="container two-column">
+        <h1>Players</h1>
+      </div>
+    );
+  }
+}
+
+export default Players;
